fac0602045278597ea881b2ab6104ab575fc628b
ksrinreddy <ksrinreddy@gmail.com>
2023-06-25 10:02:39 +0530

    get familiar with types of variables, situations, syntax and related basics

---
+++ 01intro/variableMe.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "hello ram";
+var mynum = 3;
+mynum.toUpperCase();
+greetings.toLowercase();
+console.log(greetings);
---
+++ 01intro/variableMe.ts
@@ -0,0 +1,10 @@
+let greetings: string = "hello ram";
+
+let mynum = 3;
+mynum.toUpperCase();
+
+greetings.toLowercase();
+
+console.log(greetings);
+
+export {};
