d59fa1febc847c46ef2b90488acb2599501e4e0f
ksrinreddy <ksrinreddy@gmail.com>
2023-06-25 12:26:50 +0530

    get familiar with type inference, best practices of assigning type

--- 01intro/variableMe.js
+++ 01intro/variableMe.js
@@ -1,7 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var greetings = "hello ram";
-var mynum = 3;
-mynum.toUpperCase();
-greetings.toLowercase();
+greetings.toLowerCase();
 console.log(greetings);
+// let userId:number = 39933;
+// no need to specify type here, its too redundant. (you are assigning a number to userId and typescript smartly derives type inference from the assigned value. for example like below)
+var userId = 39933;
+// userId.
+// boolean
+var isLoggedIn = false;
--- 01intro/variableMe.ts
+++ 01intro/variableMe.ts
@@ -1,10 +1,17 @@
 let greetings: string = "hello ram";
 
-let mynum = 3;
-mynum.toUpperCase();
-
-greetings.toLowercase();
-
+greetings.toLowerCase();
 console.log(greetings);
+
+// let userId:number = 39933;
+// no need to specify type here, its too redundant. (you are assigning a number to userId and typescript smartly derives type inference from the assigned value. for example like below)
+
+let userId = 39933;
+userId.toFixed();
+userId = "ram";
+
+// boolean
+let isLoggedIn: boolean = false;
+// isLoggedIn.
 
 export {};
