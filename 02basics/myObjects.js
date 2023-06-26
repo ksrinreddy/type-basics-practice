9245599a1078694c98d95c3ec3ecc439e0448593
ksrinreddy <ksrinreddy@gmail.com>
2023-06-26 12:34:37 +0530

    get familiar with type aliases

--- 02basics/myObjects.js
+++ 02basics/myObjects.js
@@ -1,26 +1,7 @@
 "use strict";
 // working with objects in typescript
 Object.defineProperty(exports, "__esModule", { value: true });
-var user = {
-    name: "ram",
-    email: "ram@email.com",
-    isActive: true,
-};
-// function parameters type
-function createUser(_a) {
-    var string = _a.name, boolean = _a.isPaid;
+function createUser(user) {
+    return { name: "", email: "", isActive: true };
 }
-// createUser({ name: "ram", isPaid: false });
-// add one more parameter to function (throws warning)
-// createUser({name: 'ram', isPaid: false, email: 'ram@email.com'}) {};
-// does not throw the warning! (odd behaviour)
-var newUser = {
-    name: "ram",
-    isPaid: false,
-    email: "ram@email.com",
-};
-createUser(newUser);
-// function return type
-function createCourse() {
-    return { name: "typescript", price: 299 };
-}
+createUser({ name: "", email: "", isActive: true });
--- 02basics/myObjects.ts
+++ 02basics/myObjects.ts
@@ -1,29 +1,43 @@
 // working with objects in typescript
 
-const user = {
-  name: "ram",
-  email: "ram@email.com",
-  isActive: true,
+// const user = {
+//   name: "ram",
+//   email: "ram@email.com",
+//   isActive: true,
+// };
+
+// // function parameters type
+// function createUser({ name: string, isPaid: boolean }) {}
+// // createUser({ name: "ram", isPaid: false });
+
+// // add one more parameter to function (throws warning)
+// // createUser({name: 'ram', isPaid: false, email: 'ram@email.com'}) {};
+
+// // does not throw the warning! (odd behaviour)
+// let newUser = {
+//   name: "ram",
+//   isPaid: false,
+//   email: "ram@email.com",
+// };
+// createUser(newUser);
+
+// // function return type
+// function createCourse(): { name: string; price: number } {
+//   return { name: "typescript", price: 299 };
+// }
+
+// TYPE ALIASES
+
+type User = {
+  name: string;
+  email: string;
+  isActive: boolean;
 };
 
-// function parameters type
-function createUser({ name: string, isPaid: boolean }) {}
-// createUser({ name: "ram", isPaid: false });
-
-// add one more parameter to function (throws warning)
-// createUser({name: 'ram', isPaid: false, email: 'ram@email.com'}) {};
-
-// does not throw the warning! (odd behaviour)
-let newUser = {
-  name: "ram",
-  isPaid: false,
-  email: "ram@email.com",
-};
-createUser(newUser);
-
-// function return type
-function createCourse(): { name: string; price: number } {
-  return { name: "typescript", price: 299 };
+function createUser(user: User): User {
+  return { name: "", email: "", isActive: true };
 }
+
+createUser({ name: "", email: "", isActive: true });
 
 export {};
