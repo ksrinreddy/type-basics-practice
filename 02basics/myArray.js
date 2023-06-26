265833f03e4ea1b102fff6bc58566dea0a37b70f
ksrinreddy <ksrinreddy@gmail.com>
2023-06-26 14:32:05 +0530

    get familiar working with arrays

---
+++ 02basics/myArray.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var superHeros = [];
+// const heroPower: number[] = [];
+var heroPower = [];
+var allUsers = [];
+// const MLModels: number[][] = [[255, 255, 255], "", []];
+var MLModels = [[255, 255, 255], []];
+superHeros.push("spiderman");
+heroPower.push(2);
+allUsers.push({ name: "", isActive: true });
